190b0028a436a47edfd37cec7ed9ce6e21715638
Moises Alejandro Alvarado Claro <moises.alvarado@globant.com>
2019-05-23 17:12:23 -0500

    linter fixes

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -1,17 +1,17 @@
 const {Command, flags} = require('@oclif/command')
-const {getWeather} = require('../utils/weather');
-const chalk = require('chalk');
+const {getWeather} = require('../utils/weather')
+const chalk = require('chalk')
 
 class WeatherCommand extends Command {
   async run() {
     const {flags} = this.parse(WeatherCommand)
     try {
       const weatherResults = await getWeather(flags.city, flags.apiKey)
-      
+
       this.log(`The weather in ${chalk.bold.green(flags.city)} is: 
         Temp: ${chalk.bold.yellow(weatherResults.main.temp)}
         Description: ${chalk.bold.yellow(weatherResults.weather[0].description)}
-      `);
+      `)
     } catch (error) {
       this.error(error.error.message)
     }
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,17 +1,17 @@
 const request = require('request-promise')
 
 async function getWeather(city, apiKey) {
-    return request({
-        method: 'get',
-        url: 'https://api.openweathermap.org/data/2.5/weather',
-        qs: {
-            q: `${city}`,
-            APPID: apiKey
-        },
-        json: true
-    });
+  return request({
+    method: 'get',
+    url: 'https://api.openweathermap.org/data/2.5/weather',
+    qs: {
+      q: `${city}`,
+      APPID: apiKey,
+    },
+    json: true,
+  })
 }
 
 module.exports = {
-    getWeather
+  getWeather,
 }
